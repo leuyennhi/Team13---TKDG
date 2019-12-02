9ba269b7cc2edec2c114ec91a5b474f8e060e73e
leuyennhi <1612470@student.hcmus.edu.vn>
2019-12-02 17:54:12 +0700

    update tryclothes page

--- Web-Shoping/controllers/clothController.js
+++ Web-Shoping/controllers/clothController.js
@@ -1,3 +1,31 @@
+var Product = require('../models/products');
+var Category = require('../models/catergories');
+var async = require('async');
+
 exports.try_clothe = function(req, res) {
-    res.render('tryclothe/index', { title: 'Thử đồ'});
+  async.parallel(
+    {
+      products: function(callback) {
+        Product.find()
+          .exec(callback);
+      },
+      categories: function(callback) {
+        Category.find().exec(callback);
+      }
+    },
+    function(err, results) {
+      if (err) {
+        return next(err);
+      }
+      results.products.forEach(ele => {
+        ele.img = ele.img[0];
+      });
+      res.render('tryclothe/index', {
+        title: 'Thử đồ',
+        products: results.products,
+        categories: results.categories,
+        user: req.user
+      });
+    }
+  );
 };
