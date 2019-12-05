2067ae3c7c69c900af1e581d0b8f651cea998d1e
duyminh256 <1612384@student.hcmus.edu.vn>
2019-12-05 20:17:40 +0700

    change model

--- Web-Shoping/public/pages/page-try-on-clothes/change-model.js
+++ Web-Shoping/public/pages/page-try-on-clothes/change-model.js
@@ -1,3 +1,16 @@
-var onClick = ()=>{
-    
+var onClickModel = () =>{
+    var x = document.getElementById("height").value;
+    var y = document.getElementById("waist").value;
+    console.log(y)
+    if (y>80){
+        $("#model").attr("src","/images/Models/3.png");
+        console.log(3)
+    }
+    else if (x>170){
+        $("#model").attr("src","/images/Models/1.png");
+        console.log(1)
+    }else{
+        $("#model").attr("src","/images/Models/2.png");
+        console.log(2)
+    }
 }
