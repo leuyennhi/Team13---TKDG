52690222ca60c199243d645fab38bcff0bbad897
leuyennhi <1612470@student.hcmus.edu.vn>
2019-12-07 01:16:43 +0700

    update form evaluation, comment

---
+++ Web-Shoping/public/pages/page-product-detail/evaluation-form.js
@@ -0,0 +1,11 @@
+$( "#open-form-btn" ).click(function() {
+  $("#evaluation-form").css('display','flex');
+});
+
+$( "#close-form-btn" ).click(function() {
+  $("#evaluation-form").hide();
+});
+
+$( "#send-form-btn" ).click(function() {
+  $("#evaluation-form").hide();
+});
---
+++ Web-Shoping/public/pages/page-product-detail/select-size-button.js
@@ -0,0 +1,14 @@
+$("#size-S").click(function() {
+  $(".btn-size").removeClass("size-selected");
+  $("#size-S").addClass("size-selected");
+});
+
+$("#size-M").click(function() {
+  $(".btn-size").removeClass("size-selected");
+  $("#size-M").addClass("size-selected");
+});
+
+$("#size-L").click(function() {
+  $(".btn-size").removeClass("size-selected");
+  $("#size-L").addClass("size-selected");
+});
