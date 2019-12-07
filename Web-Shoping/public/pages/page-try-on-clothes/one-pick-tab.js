c886850d22f86083c2087a4ea3c07f25644639d1
duyminh256 <1612384@student.hcmus.edu.vn>
2019-12-07 21:00:24 +0700

    update fit clothe

--- Web-Shoping/public/pages/page-try-on-clothes/one-pick-tab.js
+++ Web-Shoping/public/pages/page-try-on-clothes/one-pick-tab.js
@@ -1,3 +1,80 @@
+
+var img = []
+var topM = 0
+var leftM = 0
+var model = ""
+var posImg = -1
+var size = ""
+var getFigureM = ()=>{
+  var x = document.getElementById("height").value;
+  var y = document.getElementById("waist").value;
+
+  model = "/images/Models/2.png";
+  posImg = 1;
+  size = "S"
+  if (y>80){
+   model = "/images/Models/3.png";
+   posImg = 2;
+   size = "L"
+  }
+  else if (x>170){
+    model = "/images/Models/1.png";
+    posImg = 0
+    size = "M"
+  }
+}
+var getFigure = (type) =>{
+  var x = document.getElementById("height").value;
+  var y = document.getElementById("waist").value;
+  switch(type) {
+    case "shirt":
+      {
+        topM = 124
+        if (y>80){
+          topM = 130
+        }
+        else if (x>170){
+          topM = 95
+        }
+        break;
+      }
+    case "skirt":
+        {
+          topM = 224
+          if (y>80){
+            topM = 224
+          }
+          else if (x>170){
+            topM = 196
+          }
+          break;
+        }
+    case "dress":
+        {
+          topM = 134
+          if (y>80){
+            topM = 138
+          }
+          else if (x>170){
+            topM = 105
+          }
+          break;
+        }
+    default:
+      break;
+  }
+}
+var onClickModel = () =>{
+  getFigureM()
+  $("#model").attr("src",model);
+  img.forEach(ele=>{
+    getFigure(ele.type)
+    $(`#model-${ele.type}`).attr("src",`${ele.imgT[posImg]}`);
+    $(`#model-${ele.type}`).css("top",`${topM}px`);
+    $(`#model-${ele.type}`).css("left",`${leftM}px`);
+  })
+}
+
 $("input:checkbox.tab").on('click', function() {
     var $box = $(this);
     if ($box.is(":checked")) {
@@ -11,7 +88,7 @@ $("input:checkbox.tab").on('click', function() {
     var url = href.replace('detail','product-detail');
     getProduct(url)
   });
-  $('.product-selected-list').on('click','.select-product',function() {
+$('.product-selected-list').on('click','.select-product',function() {
     var $box = $(this);
     var type = $box.attr('id')
     if ($box.is(":checked")) {
@@ -31,42 +108,36 @@ var getProduct = (_url) =>{
     .done(function( data ) {
        addList(data)
        changeClothe(data)
+       img.push({type:data.type,imgT: data.toTryImg})
     });
 }
 var changeClothe = (product) =>{
-    var x = document.getElementById("height").value;
-    var y = document.getElementById("waist").value;
-    var pos = 1
-    if (y>80){
-      pos = 2
-    }
-    else if (x>170){
-      size = 0
-    }
-    $(`#model-${product.type}`).attr("src",`${product.toTryImg[pos]}`);
-    $(`#model-${product.type}`).css("display","flex");
-
+  getFigureM()
+  $(`#model-${product.type}`).attr("src",`${product.toTryImg[posImg]}`);
+  getFigure(product.type);
+  console.log(topM)
+  $(`#model-${product.type}`).css("top",`${topM}px`);
+  $(`#model-${product.type}`).css("left",`${leftM}px`);
+  $(`#model-${product.type}`).css("display","flex");
 }
 var addList = (product) =>{
-    var x = document.getElementById("height").value;
-    var y = document.getElementById("waist").value;
-    var size = 'S'
-    if (y>80){
-      size = 'M'
-    }
-    else if (x>170){
-      size = 'L'
-    }
+    getFigureM()
     var item = `<div class="product ${product.type}">
                   <h4 class="name">${product.name}</h4>
                   <h4 class="size">${size}</h4>
                   <h4 class="price">${product.price}<u>đ</u></h4>
                   <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input select-product checked" id="${product.type}">
+                    <input type="checkbox" class="custom-control-input select-product" checked id="${product.type}">
                     <label class="custom-control-label" for="${product.type}"></label>
                   </div>
                 </div>`
     $( `.${product.type}` ).remove();
     $('.product-selected-list').append(item)
     $('.selected-products button').css("display","block")
+    if(product.type == "dress"){
+      $("input:checkbox#skirt").prop("checked", false);
+      $("input:checkbox#shirt").prop("checked", false);
+      $(`#model-shirt`).css("display","none");
+      $(`#model-skirt`).css("display","none");
+    }
 }
