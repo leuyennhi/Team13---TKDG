8fd102a087c9e3ea9a00c6d0724ede1ec6bf56bc
Your Name <you@example.com>
2019-12-31 01:57:03 +0700

    add check-compare-product

--- Web-Shoping/models/products.js
+++ Web-Shoping/models/products.js
@@ -4,28 +4,28 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
-    name: {type: String, required: true},
+    name: { type: String, required: true },
     catergory: { type: Schema.ObjectId, ref: 'Catergory', required: true },
-    originalPrice: {type: Number},
-    price: {type: Number, required: true},
-    amount: {type: Number, required: true},
-    color:{type: String, required: true},
-    size:{type:String, required: true},
-    description:{type: String, required: false},
-    img:{type:[String],require: true},
-    dateImp: {type: Date, default: Date.now()},
-    watch:{type: Number,default: 0 },
-    bought:{type:Number},
-    toTryImg:{type:Array},
-    type: {type: String}
+    originalPrice: { type: Number },
+    price: { type: Number, required: true },
+    amount: { type: Number, required: true },
+    color: { type: Array, required: true },
+    size: { type: Array, required: true },
+    description: { type: String, required: false },
+    img: { type: [String], require: true },
+    dateImp: { type: Date, default: Date.now() },
+    watch: { type: Number, default: 0 },
+    bought: { type: Number },
+    toTryImg: { type: Array },
+    type: { type: String }
 });
 
 // Virtual for this Product instance URL.
 ProductSchema
-.virtual('url')
-.get(function () {
-  return '/product/'+this._id;
-});
+    .virtual('url')
+    .get(function() {
+        return '/product/' + this._id;
+    });
 
 // Export model.
 module.exports = mongoose.model('Product', ProductSchema);
--- Web-Shoping/models/reviews.js
+++ Web-Shoping/models/reviews.js
@@ -4,12 +4,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ReviewSchema = new Schema({
-    name: { type: String, required: true },
-    content: {type: String, required: true},
-    product: {type: Schema.ObjectId, required: true},
-    star: {type:String, required: true}
+    name: { type: String, required: Boolean },
+    content: { type: String, required: Boolean },
+    product: { type: Schema.ObjectId, required: Boolean },
+    star: { type: Number, required: Boolean }
 });
 
 // Export model.
 module.exports = mongoose.model('Review', ReviewSchema);
-    
